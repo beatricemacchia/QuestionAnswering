60b5c70ffee34633452ec9bd304e95b270f0df72
beatricemacchia <83776466+beatricemacchia@users.noreply.github.com>
2022-11-13 11:02:37 +0100

    adding retriever

---
+++ bertserini/utils/retriever.py
@@ -0,0 +1,24 @@
+from pyserini.search.lucene import LuceneSearcher
+from bertserini.utils.base import Context
+
+
+class Retriever:
+    def __init__(self, index):
+        self.searcher = LuceneSearcher.from_prebuilt_index(index)
+
+    def retrieve(self, question, results=10):
+        try:
+            hits = self.searcher.search(question.text, k=results)
+        except ValueError as e:
+            print(f"Failure while retrieving: {question.text}, {e}")
+
+        contexts = []
+        for i in range(0, len(hits)):
+            id = hits[i].docid
+            score = hits[i].score
+            document = self.searcher.doc(hits[i].docid)
+            text = document.raw()
+            language = 'en'
+            parag = Context(id, score, text, language)
+            contexts.append(parag)
+        return contexts
